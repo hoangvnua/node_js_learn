b120b61569d44d220cce4b371edf786fd7eaa14b
hoangvnua <nvhoang.vnua@gmail.com>
2023-03-22 22:29:35 +0700

    tổ chức mô hình mvc

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@ const app = express()
 const path = require('path');
 const port = 3000
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+route(app);
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
